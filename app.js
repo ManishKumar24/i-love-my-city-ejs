bb3bc097c2d15e443ea83b3a03cdeca3cd0ddf9e
Manish Kumar <ManishK@talentica-all.com>
2017-12-26 17:08:02 +0530

    i-love-my city

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', routes.home);
 app.get('/:city', routes.city);
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8081;
 var server = app.listen(port, function (req, res) {
     console.log("catch the action at http://localhost:" + port);
 }
